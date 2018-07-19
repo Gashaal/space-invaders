0e109387c9f05b4eb92be3b249a125cd612edd75
Dyachenko R <antinaar@gmail.com>
2018-07-19 23:40:48 +0300

    update fixtures, tests for cannon add move methods to cannon add sprite to cannon

--- __tests__/cannon.spec.js
+++ __tests__/cannon.spec.js
@@ -4,23 +4,39 @@ import Cannon from '../src/cannon';
 let cannon;
 
 beforeAll(() => {
-  cannon = new Cannon(fixtures);
+  document.body.innerHTML = '<canvas id="game-canvas" width="480" height="320"></canvas>';
+  const ctx = document.getElementById('game-canvas').getContext('2d');
+  cannon = new Cannon(ctx, fixtures);
 });
 
 test('store equal fixtures', () => {
   expect(cannon.store).toEqual(fixtures);
 });
 
-test('run calc center in constructor', () => {
-  expect(cannon.store.cannon.x).toBe(225);
-  expect(cannon.store.cannon.y).toBe(275);
+test('run calc initial coords in constructor', () => {
+  expect(cannon.store.cannon.x).toBe(369);
+  expect(cannon.store.cannon.y).toBe(552);
 });
 
 test('calc cannon center coords', () => {
   cannon.store.cannon.x = 0;
   cannon.store.cannon.y = 0;
 
-  cannon.calcCenter();
-  expect(cannon.store.cannon.x).toBe(225);
-  expect(cannon.store.cannon.y).toBe(275);
+  cannon.calcInitialCoords();
+  expect(cannon.store.cannon.x).toBe(369);
+  expect(cannon.store.cannon.y).toBe(552);
+});
+
+test('move right', () => {
+  const x = cannon.store.cannon.x;
+
+  cannon.moveRight();
+  expect(cannon.store.cannon.x).toBe(x + cannon.store.cannon.dx);
+});
+
+test('move left', () => {
+  const x = cannon.store.cannon.x;
+
+  cannon.moveLeft();
+  expect(cannon.store.cannon.x).toBe(x - cannon.store.cannon.dx);
 });
--- src/cannon.js
+++ src/cannon.js
@@ -1,15 +1,41 @@
 class Cannon {
-  constructor(store) {
+  constructor(ctx, store) {
+    this.ctx = ctx;
     this.store = store;
-    this.calcCenter();
+
+    this.calcInitialCoords();
   }
 
-  calcCenter() {
-    const cannon = this.store.cannon;
+  calcInitialCoords() {
+    const {width, height} = this.store.cannon;
 
-    this.store.cannon.x = this.store.ctxWidth/2 - cannon.width/2;
-    this.store.cannon.y = this.store.ctxHeight - cannon.height - cannon.height/2;
+    this.store.cannon.x = this.store.ctxWidth/2 - width/2;
+    this.store.cannon.y = this.store.ctxHeight - height - height/2;
+  }
+
+  draw() {
+    const {
+      sprite,
+      cannon: {
+        sprite: {sX, sY, sW, sH},
+        x: dx,
+        y: dy,
+        width: dWidth,
+        height: dHeight,
+      },
+    } = this.store;
+
+    this.ctx.drawImage(sprite, sX, sY, sW, sH, dx, dy, dWidth, dHeight);
+  }
+
+  moveRight() {
+    this.store.cannon.x += this.store.cannon.dx;
+  }
+
+  moveLeft() {
+    this.store.cannon.x -= this.store.cannon.dx;
   }
 }
+
 
 export default Cannon;
